f7369a2ab2fb0181a602e2ac67ff1c4e9e7275c2
nohaylees <hayleemitt@gmail.com>
2024-10-10 16:20:38 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered!', icon="✅")
-
-#requirements.txt
-snowflake-connector-python
-snowflake-snowpark-python
